675ac5bef2b1ef97a80d1b8dfe6d7c02910ed7a1
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-08-26 17:26:28 +0200

    Changed number format into class

--- number_format_parser.py
+++ number_format_parser.py
@@ -1,49 +1,67 @@
 #!/usr/bin/env python3
 """
-Get number format of a string
+Class to store format of numbers
+can also parse strings to determine their format
 """
 
-def number_format_parse(number):
-    # all variables here, could be turned into module
-    fmt_char = 'f'
-    is_negative = False
-    int_length = 0
-    dec_length = 0
+class NumberFmt:
+    def __init__(self):
+        self.flag = '-'
+        self.specifier = 'f'
+        self.width = 12
+        self.precision = 6
 
-    try:
-        float(number)
-    except ValueError:
-        print('String is not a number')
+    def parser(self, number):
+        """Parse format of number string"""
+        is_signed = False
 
-    if number[0] == '-':
-        is_negative = True
+        try:
+            float(number)
+        except ValueError:
+            print('String is not a number')
 
-    dot_position = number.find('.')
-    if dot_position == -1:
-        fmt_str = "%{}.0f".format(len(number) - is_negative)
+        if number[0] == '+':
+            is_signed = True
+            self.flag ='+'
+        elif number[0] == '-':
+            is_signed = True
+            # can't say anything about the flag then
+
+        dot_position = number.find('.')
+        if dot_position == -1: # number is an integer
+            self.precision = 0
+            self.width = len(number) - is_signed
+            return
+
+        self.width = len(number) - is_signed - 1
+
+        # check if exponential if dot was at 2nd position
+        if dot_position - is_signed == 1:
+            for e in ['e', 'E']:
+                e_position = number.find(e)
+                if e_position != -1:
+                    self.specifier = e
+                    self.precision = e_position - dot_position - 1
+                    break
+
+        self.precision = len(number) - dot_position - 1
+        return
+
+
+    def get(self):
+        """Return format as C style string"""
+        fmt_str = "%{}{}.{}{}".format(self.flag, self.width, self.precision, self.specifier)
         return fmt_str
 
-    int_length = dot_position - is_negative
 
-    # check if exponential if dot was in 2nd (with minus 3rd) character
-    if dot_position - is_negative == 1:
-        for e in ['e', 'E']:
-            e_position = number.find(e)
-            if e_position != -1:
-                fmt_char = e
-                dec_length = e_position - dot_position - 1
-                break
+    # def set(self, format_string):
 
-    if fmt_char == 'f':
-        dec_length = len(number) - dot_position - 1
-
-    fmt_str = "%{}.{}{}".format(int_length, dec_length, fmt_char)
-    return fmt_str
 
 
 if __name__ == '__main__':
     numbers = ['21.5624', '1.6345e-5', '6.276E2', '3122225', '-2.43123', '-5.781234e10', '-63214']
     for num in numbers:
-        fmt_str = number_format_parse(num)
-        formatted_num = fmt_str % (float(num))
-        print("{}: {} -> {}".format(num, fmt_str, formatted_num))
+        fmt = NumberFmt()
+        fmt.parser(num)
+        formatted_num = fmt.get() % (float(num))
+        print("{}: {} -> {}".format(num, fmt.get(), formatted_num))
