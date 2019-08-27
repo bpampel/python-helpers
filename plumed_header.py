2e21febff35b8bbcf8b23ed5d68a543c2d96e20d
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-08-27 14:59:39 +0200

    Added some more methods to manipulate the header

--- plumed_header.py
+++ plumed_header.py
@@ -11,7 +11,7 @@ class PlumedHeader:
         self.data = []
 
 
-    def __str__(self):
+    def string(self):
         """
         Returns header as string with newlines to be printed to file
         Can be used directly as header argument to numpys savetxt
@@ -30,6 +30,7 @@ class PlumedHeader:
                 header.append(line)
             else:
                 self.data = header
+                return
         raise ValueError("End of file reached. Is this really a plumed data file?")
 
 
@@ -44,9 +45,40 @@ class PlumedHeader:
             self.data.insert(pos, line + '\n')
 
 
+    def replace_line(self, line, pos):
+        """
+        Replace header line at given position (line number starting with 0)
+        """
+        if pos >= len(self.data) or pos < 0:
+            raise ValueError("Trying to replace a nonexistant header line.")
+        self.data[pos] = line
+
+
+    def del_line(self, pos):
+        """
+        Delete header line at given position (line number starting with 0)
+        """
+        del self.data[pos]
+
+
     def set(self, header):
         """
         Set header to given list of strings
         Will overwrite existing header
         """
         self.data = header
+
+
+    def get_fields(self):
+        """
+        Get column titles from the FIELDS line
+        """
+        return [field[2:] for field in [line.split() for line in self.data] if field[1] == "FIELDS"]
+
+
+    def search_lines(self, string):
+        """
+        Get all lines containing string
+        Returns list of tuples (linenumber, line)
+        """
+        return [(i, line) for i, line in enumerate(self.data) if string in line]
