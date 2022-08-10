7c3e22ffc3f68a30c38dcacde33be2ffd57c6bad
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2022-08-10 11:55:47 +0200

    Small changes to plot function

--- plot.py
+++ plot.py
@@ -20,13 +20,15 @@ def plot_data(
     xvals and yvals can be either simple lists of values to plot a single function
     or they can be lists of lists with each list being a function to plot.
 
+    If only one list of xvals is specified it is used for all yvals
+
     This currently doesn't check if all lengths match,
     expect errors if e.g. the "plot_labels" list is shorter than the xvals list
 
     param xvals: x values of functions
     param yvals: y values of functions
     param styles: specify color and other properties passed to plot for each function
-    param plot_labels: labels for the functions
+    param func_labels: labels for the functions
     param ylabel: label for y axis
     param xlabel: label for x axis
     param plot_title: title of plot
@@ -35,8 +37,16 @@ def plot_data(
     fig = plt.figure(figsize=(8, 4), dpi=100)
     ax = plt.axes()
     try:
-        iter(xvals[0])  # check if xval is list of lists --> multiple functions
+        iter(yvals[0])  # check if xval is list of lists --> multiple functions
         # set default lists of right length if not specified
+        try:
+            iter(xvals[0])
+            if len(yvals) != len(xvals):
+                raise ValueError(
+                    "Specify either xvals for all yvals or a single list for all"
+                )
+        except TypeError:  # only one specified, use same for all
+            yvals = [yvals] * len(xvals)
         if not styles:
             styles = [""] * len(xvals)
         if not func_labels:
@@ -56,5 +66,6 @@ def plot_data(
     if filename:
         fig.savefig(filename)
     else:
-        plt.show(-1)
+        plt.tight_layout()
+        plt.show(block=True)
     plt.close(fig)
